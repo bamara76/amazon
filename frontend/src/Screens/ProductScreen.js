2c9f14a8459e82192640c1c831abfdb5a6d72854
bamara76 <louvetot76@gmail.com>
2020-11-02 22:44:58 +0100

    add existing file

---
+++ frontend/src/Screens/ProductScreen.js
@@ -0,0 +1,13 @@
+import react from 'react';
+import data from '../data';
+
+
+function ProductScreen(props) {
+    console.log(props.match.params.id);
+    const product = data.products.find(x=> x.id === props.match.params.id);
+    return <div>
+        <h1>{product.name}</h1>
+    </div>
+}
+
+export default ProductScreen;
---
+++ frontend/src/data.js
@@ -0,0 +1,45 @@
+export default {
+    products:[
+    {
+        id:'1',
+        name:'Slim Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        numReviews:10
+    },
+
+    {
+        id:'2',
+        name:'Fit Shirt',
+        category:'Shirts',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        numReviews:10
+    },
+
+    {
+        id:'3',
+        name:'Best Pants',        
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        numReviews:10
+    },
+    {
+        id:'4',
+        name:'Best Pants',        
+        category:'Pants',
+        image:'/images/d1.jpg',
+        price:60,
+        brand:'Nike',
+        numReviews:10
+    },
+
+
+    ]
+
+}
